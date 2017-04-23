e776f696aa75aed15903c9ff4a4622319d903367
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-04-23 14:43:46 -0500

    calculator functionality working

--- public/client.js
+++ public/client.js
@@ -2,4 +2,44 @@ $(document).ready(onReady);
 
 function onReady () {
   console.log( 'JQ' );
-}
+  // event listeners
+  $('#math-operation-nav').on('click', 'button', mathOperationSelector);
+
+} // end onReady
+
+// create objectToSend from input field 1 and 2 and selected operation
+function mathOperationSelector () {
+  console.log($('#input-1').val());
+  console.log($('#input-2').val());
+  console.log('in mathOperationSelector:', $(this).data('operator'));
+
+  var objectToSend = {
+    input1: $('#input-1').val(),
+    input2: $('#input-2').val(),
+    operationType: $(this).data('operator')
+  };
+  console.log(objectToSend);
+
+  // send to server at '/calc'
+  $.ajax({
+    url: '/calc',
+    type: 'POST',
+    data: objectToSend,
+    success: function (response) {
+      console.log('Back from server with:', response);
+      getResult();
+    } // end success
+  }); // end ajax POST
+} // end mathOperationSelector
+
+// receive from server at '/result'
+function getResult () {
+  $.ajax({
+    url: '/result',
+    type: 'GET',
+    success: function (response) {
+      $('#answer').empty();
+      $('#answer').append(response.result);
+    } // end success
+  }); // end ajax GET
+} // end getResult
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var path = require( 'path' );
 var bodyParser = require( 'body-parser' );
 
 // globals
-var port = 3005;
+var port = 3006;
+var result = {
+
+};
 
 // uses
 app.use( express.static( 'public' ) );
@@ -22,3 +25,48 @@ app.get( '/', function (req, res) {
   // send back base url
   res.sendFile( path.resolve( 'public/views/index.html' ) );
 });
+
+// calc URL
+app.post('/calc', function (req, res) {
+  console.log('calc url hit, passed:', req.body);
+  result = makeCalculation(req.body);
+  console.log('result:', result);
+  res.sendStatus(200);
+});
+
+// return result
+app.get('/result', function (req, res) {
+  console.log('result url hit');
+  // create return object
+  var objectToReturn = {
+    result: result
+  };
+  res.send(objectToReturn);
+});
+
+// function to make the calculations
+function makeCalculation (mathObject) {
+  var mathResult;
+  // check to make sure only numbers were entered
+  if ( isNaN(mathObject.input1) || isNaN(mathObject.input2) || mathObject.input1 === '' || mathObject.input2 === '' ) {
+    return 'can not be calculated';
+  } // end if
+  switch (mathObject.operationType) {
+    case '+':
+      mathResult = parseFloat(mathObject.input1) + parseFloat(mathObject.input2);
+      break;
+    case '-':
+      mathResult = parseFloat(mathObject.input1) - parseFloat(mathObject.input2);
+      break;
+    case '*':
+      mathResult = parseFloat(mathObject.input1) * parseFloat(mathObject.input2);
+      break;
+    case '/':
+      mathResult = parseFloat(mathObject.input1) / parseFloat(mathObject.input2);
+      break;
+    default:
+      return 'can not be calculated';
+  } // end switch
+  console.log('mathResult', mathResult);
+  return mathResult;
+} // end makeCalculation
