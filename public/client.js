3e696c28551e3d5eef17b40e24880fedb12bbeaa
Ben Lauderbaugh <benjlaud1@gmail.com>
2017-04-23 22:11:47 -0500

    completed hard mode assignment

--- public/client.js
+++ public/client.js
@@ -1,13 +1,22 @@
 $(document).ready(onReady);
 
+// Global variables
+var objectToPush = {};
+var noPeriod = true;
+
 function onReady () {
   console.log( 'JQ' );
   // event listeners
   $('#math-operation-nav').on('click', 'button', mathOperationSelector);
+  $('#input-keys').on('click', '.number-pad', numberSelector);
+  $('#input-keys').on('click', '.operator', operatorSelector);
+  $('#equals').on('click', equalsSelector);
 
+  clearInputFields();
 } // end onReady
 
 // create objectToSend from input field 1 and 2 and selected operation
+// this function was used for the base mode. I created new functions for the hard mode
 function mathOperationSelector () {
   console.log($('#input-1').val());
   console.log($('#input-2').val());
@@ -26,26 +35,71 @@ function mathOperationSelector () {
     type: 'POST',
     data: objectToSend,
     success: function (response) {
-      console.log('Back from server with:', response);
+      // console.log('Back from server with:', response);
       getResult();
     } // end success
   }); // end ajax POST
 } // end mathOperationSelector
 
-// receive from server at '/result'
+// receive result from server at '/result'
 function getResult () {
   $.ajax({
     url: '/result',
     type: 'GET',
     success: function (response) {
-      $('#answer').empty();
-      $('#answer').append(response.result);
+      // base mode code below
+      // $('#answer').empty();
+      // $('#answer').append(response.result);
       clearInputFields();
+      $('#screen').val(response.result);
     } // end success
   }); // end ajax GET
 } // end getResult
 
+// clear the #screen
 function clearInputFields () {
-  $('#input-1').val('');
-  $('#input-2').val('');
-}
+  // base mode code below
+  // $('#input-1').val('');
+  // $('#input-2').val('');
+  $('#screen').val('');
+} // end clearInputFields
+
+// record number button presses and append to the screen
+function numberSelector () {
+  // console.log('In number selector:', $(this).data('number'));
+  // prevent multiple periods
+  if (($(this).data('number')).toString() === '.' && noPeriod ) {
+    noPeriod = false;
+  } else if (($(this).data('number')).toString() === '.' && !noPeriod) {
+    return;
+  }
+  // add the number of period to the #screen
+  $('#screen').val($('#screen').val() + ($(this).data('number')).toString());
+} // end numberSelector
+
+// capture the first number and the mathmatical operator
+function operatorSelector () {
+  // console.log('In operator selector:', $(this).data('operator'));
+  objectToPush.input1 = parseFloat($('#screen').val());
+  objectToPush.operationType = $(this).data('operator');
+  // clear the #screen
+  $('#screen').val('');
+  // reset period checking to allow next number to have a period
+  noPeriod = true;
+} // end operatorSelector
+
+// capture the second number and send the object to the server for calculations
+function equalsSelector () {
+  objectToPush.input2 = parseFloat($('#screen').val());
+  // console.log('In equals selector:', objectToPush);
+  // send to server at '/calc'
+  $.ajax({
+    url: '/calc',
+    type: 'POST',
+    data: objectToPush,
+    success: function (response) {
+      // console.log('Back from server with:', response);
+      getResult();
+    } // end success
+  }); // end ajax POST
+} // end equalsSelector
